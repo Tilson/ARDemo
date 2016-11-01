9627aa2fdb2808fef38aa315ba5b58a254d54340
Tilson Lai <Tilson.Lai@aver.com>
2016-11-01 17:46:05 +0800

    add files

---
+++ ARSimpleNativeCars/gen/org/artoolkit/ar/samples/ARSimpleNativeCars/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.artoolkit.ar.samples.ARSimpleNativeCars;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
